5a040ddd734a2a3c43faa63f837ee250a6c98d85
Valentin Vasilev <VVVasilev@nemetschek.bg>
2022-02-17 17:03:11 +0200

    Added MovieInfo component and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Login from './components/Login/Login';
+import MovieInfo from './components/MovieInfo/MovieInfo';
 import Register from './components/Register/Register';
 
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/movieInfo" element={<MovieInfo />} />
       </Routes>
     </BrowserRouter>
   );
