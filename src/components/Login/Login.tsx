b69be845e8cf89a92bda72285038b055438c2340
Bogomil Slavkov <BSlavkov@nemetschek.bg>
2022-02-24 10:42:30 +0200

    register, login ui

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,45 @@
+import { Button, CardActions, TextField } from '@mui/material';
 import React, { FC } from 'react';
 import styles from './Login.module.scss';
 
-interface LoginProps {}
-
+interface LoginProps {
+ 
+}
+    
 const Login: FC<LoginProps> = () => (
   <div className={styles.Login}>
-    Login Component
+    <div >
+            
+            <span className={styles.sign_in}><h1>Sign In</h1></span>
+            
+            <TextField className={styles.email}
+              fullWidth
+              label="Email"
+          id="filled-size-normal"
+          defaultValue=""
+          variant="filled"
+            />
+            </div>
+            <div >
+            <TextField className={styles.password}
+              fullWidth
+              label="Password"
+          id="filled-size-normal"
+          defaultValue=""
+          variant="filled"
+          type = "password"
+            />
+          </div>
+          <CardActions>
+          <Button className={styles.login}
+          fullWidth
+            variant="contained"
+            size="large">
+              
+              <span><h1>SIGN IN</h1></span>
+          </Button>
+          </CardActions>
+
   </div>
 );
 
--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,11 +1,62 @@
 import React, { FC } from 'react';
 import styles from './Register.module.scss';
+import { Button, CardActions, TextField } from '@mui/material';
 
 interface RegisterProps {}
 
 const Register: FC<RegisterProps> = () => (
-  <div className={styles.Register}>
-    Register Component
+  <div className={styles.Login}>
+    <div >
+            
+            <span className={styles.sign_in}><h1>Create an account</h1></span>
+            
+            <TextField className={styles.email}
+              fullWidth
+              label="Email"
+          id="filled-size-normal"
+          defaultValue=""
+          variant="filled"
+            />
+            <div >
+            <TextField className={styles.username}
+              fullWidth
+              label="Username"
+          id="filled-size-normal"
+          defaultValue=""
+          variant="filled"
+            />
+            </div>
+            </div>
+            <div >
+            <TextField className={styles.password}
+              fullWidth
+              label="Password"
+          id="filled-size-normal"
+          defaultValue=""
+          variant="filled"
+          type = "password"
+            />
+          </div>
+          <div >
+            <TextField className={styles.confirmPassword}
+              fullWidth
+              label="Confirm Password"
+          id="filled-size-normal"
+          defaultValue=""
+          variant="filled"
+          type = "password"
+            />
+          </div>
+          <CardActions>
+          <Button className={styles.login}
+          fullWidth
+            variant="contained"
+            size="large">
+              
+              <span><h1>SIGN IN</h1></span>
+          </Button>
+          </CardActions>
+
   </div>
 );
 
