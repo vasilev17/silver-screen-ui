a900b75e124ccd40406ebbe01091646da99097e4
Boris Stefanov <bstefanov@nemetschek.bg>
2022-03-09 14:45:44 +0200

    Created window component for admin page. Buttons are now functional.

---
+++ src/components/Administration/AdminWindowComponent/AdminWindowComponent.tsx
@@ -0,0 +1,51 @@
+import React, { FC } from 'react';
+import styles from './AdminWindowComponent.module.scss';
+import QueuePlayNextIcon from '@mui/icons-material/QueuePlayNext';
+import GavelIcon from '@mui/icons-material/Gavel';
+import DoDisturbIcon from '@mui/icons-material/DoDisturb';
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
+
+interface AdminWindowComponentProps {
+  title:string,
+  iconSet:number,
+  padding,
+  height,
+}
+
+const AdminWindowComponent: FC<AdminWindowComponentProps> = (winInfo) => {
+
+  function DisplayIcon(){
+    switch(winInfo.iconSet){
+      case 1: 
+        return(
+          <QueuePlayNextIcon style={{width: '2rem', height: '2rem'}}/>
+        );
+      case 2: 
+        return(
+          <GavelIcon style={{width: '2rem', height: '2rem'}}/>
+        );
+      case 3: 
+        return(
+          <DoDisturbIcon style={{width: '2rem', height: '2rem'}}/>
+        );
+      case 4: 
+        return(
+          <AdminPanelSettingsOutlinedIcon style={{width: '2rem', height: '2rem'}}/>
+        );
+    }
+  }
+
+  return(
+  <div className={styles.AdminWindowComponent} style={{height: winInfo.height}}>
+    <div className={styles.AdminWindowComponent_banner}>
+      <div className={styles.AdminWindowComponent_banner_title}>{winInfo.title}</div>
+      <div className={styles.AdminWindowComponent_banner_icon}>{DisplayIcon()}</div>
+    </div>
+    <div style={{padding: winInfo.padding}}>
+      {winInfo.children}
+    </div>
+  </div>
+  );
+}
+
+export default AdminWindowComponent;
