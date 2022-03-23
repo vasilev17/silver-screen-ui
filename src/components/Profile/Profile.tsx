749c122dfb0b9a08ad2d14314e0d8cebfc8bdc03
Bogomil Slavkov <BSlavkov@nemetschek.bg>
2022-03-23 17:06:58 +0200

    add friend by username

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Box, Button, Fade, Grow, Modal, TextField, Typography } from '@mui/material';
+import { Avatar, Box, Button, Fade, Grow, IconButton, Input, Modal, Stack, styled, TextField, Typography } from '@mui/material';
 import React, { FC, SyntheticEvent, useEffect, useState } from 'react';
 import styles from './Profile.module.scss';
 import { useNavigate } from 'react-router-dom';
@@ -6,7 +6,8 @@ import Backdrop from '@mui/material/Backdrop';
 import Login from '../Login/Login';
 import SearchIcon from '@mui/icons-material/Search';
 import FriendList from '../FriendList/FriendList';
-
+import UploadAvatar from '../Profile/UploadAvatar/UploadAvatar';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 const style = {
   position: 'absolute',
   top: '50%',
@@ -41,12 +42,19 @@ const Profile: FC<ProfileProps> = () => {
     navigate("/");
     window.location.reload();
   }
+  function SendToAddFriend() {
+    navigate("/addfriend");
+    window.location.reload();
+
+  }
   const [username, setUsername] = useState(null);
   const [avatar, setAvatar] = useState(null);
   const navigate = useNavigate();
 
   const [code, setCode] = useState(<></>);
-
+  const Input = styled('input')({
+    display: 'none',
+  });
   var token = localStorage.getItem('token');
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API}/User/UserGetInfo`, {
@@ -56,7 +64,7 @@ const Profile: FC<ProfileProps> = () => {
         'Accept': 'application/json',
         'Authorization': `Bearer ${token}`,
       },
-     
+
     })
       .then(response => {
         if (response.ok) {
@@ -71,37 +79,48 @@ const Profile: FC<ProfileProps> = () => {
 
         setUsername(data.username);
         setAvatar(data.avatar);
-        
 
-        if (data!==null) {
-          
+
+        if (data !== null) {
+
           setCode(
+            <form onSubmit={submit}>
+              <div className={styles.frame2}>
+                <div className={styles.logout}>
+                  <Button variant="outlined" type="button" onClick={() => RemoveToken()}>Logout</Button>
+                </div>
+                <div className={styles.username} >
 
-            <div className={styles.frame2}>
-              <div className={styles.logout}>
-                <Button variant="outlined" type="button" onClick={() => RemoveToken()}>Logout</Button>
-              </div>
-              <div  className={styles.username} >
-              {data.username}
-              </div>
-              <div className={styles.avatar}>
-              <Avatar src={data.avatar}/>
-              </div>
-              <div className={styles.friendsList}>
-              <div className={styles.friends}>
-              <h1>Friends</h1>
-              </div>
-              <div className={styles.search}>
-              <SearchIcon />
-              </div>
-              </div>
-              <div className={styles.friendsList2}>
-              <FriendList/>
-              </div>
-              
-            </div>
+                  {data.username}
 
 
+                </div>
+
+                <div className={styles.avatar}>
+                  
+                        <Avatar src={data.avatar} />
+
+                      
+
+                </div>
+
+                <div className={styles.friendsList}>
+                  <div className={styles.friends}>
+                    <h1>Friends</h1>
+                  </div>
+                  <div className={styles.addFriend}>
+                    <IconButton onClick={() => SendToAddFriend()}>
+                      <PersonAddIcon />
+                    </IconButton>
+                  </div>
+                </div>
+                <div className={styles.friendsList2}>
+                  <FriendList />
+                </div>
+
+              </div>
+
+            </form>
           );
           console.log("logout")
         } else {
@@ -148,8 +167,46 @@ const Profile: FC<ProfileProps> = () => {
         );
 
       });
+
+
   }, [])
 
+
+  const [uploadavatar, setUploadAvatar] = useState(' ');
+  var token = localStorage.getItem('token');
+  const submit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    await fetch(`${process.env.REACT_APP_API}/User/UploadAvatar`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+
+      body: JSON.stringify({
+        uploadavatar
+
+      })
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          alert("Error while processing the request!");
+        }
+      }).then(data => {
+        console.log(data.token);
+        window.location.reload();
+
+
+
+
+
+      });
+
+
+  }
 
 
 
@@ -164,8 +221,6 @@ const Profile: FC<ProfileProps> = () => {
 
 
   );
-
-
 
 
 
