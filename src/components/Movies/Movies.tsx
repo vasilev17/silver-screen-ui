e543793a10e14acada84e538b0c9bc260722c13e
Kristian Valchev <KValchev@nemetschek.bg>
2022-03-31 23:01:06 +0300

    changes to genres

--- src/components/Movies/Movies.tsx
+++ src/components/Movies/Movies.tsx
@@ -11,17 +11,16 @@ function Movies() {
     <MovieRow genre="Family" content="Movie" showGenreTittle={true}/>
     <MovieRow genre="History" content="Movie" showGenreTittle={true}/>
     <MovieRow genre="Mystery" content="Movie" showGenreTittle={true}/>
+    <MovieRow genre="Sci-Fi" showGenreTittle={true}/>
     <MovieRow genre="Science Fiction" content="Movie" showGenreTittle={true}/>
     <MovieRow genre="War" content="Movie" showGenreTittle={true}/>
     <MovieRow genre="Adventure" content="Movie" showGenreTittle={true}/>
     <MovieRow genre="Crime" content="Movie" showGenreTittle={true}/>
     <MovieRow genre="Fantasy" content="Movie" showGenreTittle={true}/>
     <MovieRow genre="Horror" content="Movie" showGenreTittle={true}/>
-    <MovieRow genre="Sport" content="Movie" showGenreTittle={true}/>
     <MovieRow genre="Western" content="Movie" showGenreTittle={true}/>
     <MovieRow genre="Animation" content="Movie" showGenreTittle={true}/>
     <MovieRow genre="Documentary" content="Movie" showGenreTittle={true}/>
-    <MovieRow genre="Biography" content="Movie" showGenreTittle={true}/>
     <MovieRow genre="Drama" content="Movie" showGenreTittle={true}/>
     <MovieRow genre="Romance" content="Movie" showGenreTittle={true}/>
     <MovieRow genre="Thriller" content="Movie" showGenreTittle={true}/>
--- src/components/Series/Series.tsx
+++ src/components/Series/Series.tsx
@@ -10,20 +10,16 @@ function Series() {
     <MovieRow genre="Family" content="TVSeries" showGenreTittle={true}/>
     <MovieRow genre="History" content="TVSeries" showGenreTittle={true}/>
     <MovieRow genre="Mystery" content="TVSeries" showGenreTittle={true}/>
-    <MovieRow genre="Science Fiction" content="TVSeries" showGenreTittle={true}/>
+    <MovieRow genre="Sci-Fi" showGenreTittle={true}/>
     <MovieRow genre="War" content="TVSeries" showGenreTittle={true}/>
     <MovieRow genre="Adventure" content="TVSeries" showGenreTittle={true}/>
     <MovieRow genre="Crime" content="TVSeries" showGenreTittle={true}/>
     <MovieRow genre="Fantasy" content="TVSeries" showGenreTittle={true}/>
-    <MovieRow genre="Horror" content="TVSeries" showGenreTittle={true}/>
-    <MovieRow genre="Sport" content="TVSeries" showGenreTittle={true}/>
     <MovieRow genre="Western" content="TVSeries" showGenreTittle={true}/>
     <MovieRow genre="Animation" content="TVSeries" showGenreTittle={true}/>
     <MovieRow genre="Documentary" content="TVSeries" showGenreTittle={true}/>
-    <MovieRow genre="Reality-TV" content="TVSeries" showGenreTittle={true}/>
-    <MovieRow genre="Biography" content="TVSeries" showGenreTittle={true}/>
+    <MovieRow genre="Reality" content="TVSeries" showGenreTittle={true}/>
     <MovieRow genre="Drama" content="TVSeries" showGenreTittle={true}/>
-    <MovieRow genre="Game-Show" content="TVSeries" showGenreTittle={true}/>
     <MovieRow genre="Romance" content="TVSeries" showGenreTittle={true}/>
     <MovieRow genre="Thriller" content="TVSeries" showGenreTittle={true}/>
 
