657bd40d55e3726fd53c6e71f223d1001c47c5a7
Valentin Vasilev <VVVasilev@nemetschek.bg>
2022-04-07 12:22:16 +0300

    Removed more parts of saving scroll

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -92,7 +92,7 @@ const Navbar = () => {
     return (
         <div id="mainNavbar" className={color ? "header header_bg" : "header"}>
             <nav className='navbar'>
-                <a href='/' className='logo' onClick={() => sessionStorage.removeItem("mainPageScrollPosition")}>
+                <a href='/' className='logo'>
                     <img src="/logoS.png" alt='logo' />
                 </a>
                 <div className='hamburger' onClick={handleClick}>
